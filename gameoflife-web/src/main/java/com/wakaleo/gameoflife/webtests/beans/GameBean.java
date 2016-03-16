2ce6d13f97522fed308a72ef92989f824ae7ae8b
Romydekoning <romydekoning@outlook.com>
2016-03-16 21:40:20 +0100

    update

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/beans/GameBean.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/beans/GameBean.java
@@ -21,7 +21,8 @@ import javax.servlet.http.HttpServletRequest;
 @SessionScoped
 public class GameBean implements Serializable {
 
-    //test
+    //test jenkins
+    
     private int rows = 3;
     private int columns = 3;
     private Universe universe;
